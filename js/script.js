862315ef5f0dfebc4f23d3178eec282fb1e8823c
francs2812 <emiliofrangella2812@gmail.com>
2021-05-18 18:37:20 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -4,17 +4,32 @@ var  app = new Vue (
         data: {
             images :[
                 "https://petfamily.it/wp-content/uploads/2020/05/pastore-tedesco-in-posa.jpg",
-               "https://www.zooplus.it/magazine/wp-content/uploads/2018/12/cane-corso.jpg"
-                
+               "https://www.zooplus.it/magazine/wp-content/uploads/2018/12/cane-corso.jpg",
+               "https://www.razzedicani.net/wp-content/uploads/2016/03/san-bernardo-e1466581666914.jpg",
+              "https://i.pinimg.com/originals/bc/b9/58/bcb95857be8178a07be7367e5ed69c2f.jpg"       
             ],
-
-            index : 0
+            indexs : 0,
+    
         },
 
         methods: {
             left: function() {
-               index = index++;
-               console.log(index);
+                this.indexs--;
+                if (this.indexs == -1) {
+                    this.indexs = this.images.length - 1;
+                }
+            },
+            right: function() {
+                this.indexs++;
+                if(this.indexs == this.images.length){
+                    this.indexs = 0
+                }
+            },
+            active : function() {
+                if(index == this.indexs){
+                   this.active=true 
+                }
+                console.log(this.active);
             }
 
         }
